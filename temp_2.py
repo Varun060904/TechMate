c061296935d4f99107b7653a10bcb93b837f4d5a
Varun Ramesh <varunramesh@Varuns-MacBook-Air.local>
2024-03-13 13:33:44 +0530

    initial commit

--- temp_2.py
+++
@@ -1,8 +0,0 @@
-import ollama
-stream = ollama.chat(
-    model='llama2',
-    messages=[{'role': 'user', 'content': f"Given a user response to the following question about the file #outputtxt: Summarize the file."}],
-    stream=True,
-)
-for chunk in stream:
-        print(chunk['message']['content'])
